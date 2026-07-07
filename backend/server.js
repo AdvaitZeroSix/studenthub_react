6608d853a8618af6c9d38dd624953ef08046cdaa
AdvaitZeroSix <saxenaadvait23@gmail.com>
2026-07-07 23:34:11 +0530

    MongoDB and Route

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 
 const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log(" Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(" MongoDB Connection Error:", err);
+    });
 
 app.get("/", (req, res) => {
     res.send("StudentHub Backend is Running!");
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
